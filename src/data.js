e8ce1bb192a1870e278a9add488eee2331fc503c
Lenovo <raintruwalker@gmail.com>
2022-10-15 19:39:49 +0300

    feat: add Announcement, Navbar, Slider components

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "https://images.deal.by/166636703_w600_h600_166636703.jpg",
+        title: "Летняя распродажа",
+        desc: "Не бойтесь экспериментировать! Получите скидку 30% на новые поступления.",
+        bg: "f5fafd",
+    },
+    {
+        id: 2,
+        img: "https://pngimg.com/uploads/dresser/dresser_PNG25.png",
+        title: "Зимняя распродажа",
+        desc: "Не бойтесь экспериментировать! Получите скидку 30% на новые поступления.",
+        bg: "fcf1ed",
+    },
+    {
+        id: 3,
+        img: "https://www.freepnglogos.com/uploads/sofa-png/sofa-drawing-pencil-color-rose-png-clipart-picture-clipartlym-34.png",
+        title: "Популярно",
+        desc: "Не бойтесь экспериментировать! Получите скидку 30% на новые поступления.",
+        bg: "fbf0f4"
+    }
+]
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import Navbar from "../components/Navbar";
+import Announcement from "../components/Announcement";
+import Slider from "../components/Slider";
 
 const Home = () => {
     return (
         <div>
-            Homepage
+            <Announcement/>
+            <Navbar/>
+            <Slider/>
         </div>
     )
 }
