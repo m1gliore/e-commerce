0a1424668b38f393d814af8bf00271889c83e0ea
Lenovo <raintruwalker@gmail.com>
2022-10-16 17:11:55 +0300

    feat: add Categories, CategoryItem, Products, Product components

--- src/data.js
+++ src/data.js
@@ -4,14 +4,14 @@ export const sliderItems = [
         img: "https://images.deal.by/166636703_w600_h600_166636703.jpg",
         title: "Летняя распродажа",
         desc: "Не бойтесь экспериментировать! Получите скидку 30% на новые поступления.",
-        bg: "f5fafd",
+        bg: "f5fafd"
     },
     {
         id: 2,
         img: "https://pngimg.com/uploads/dresser/dresser_PNG25.png",
         title: "Зимняя распродажа",
         desc: "Не бойтесь экспериментировать! Получите скидку 30% на новые поступления.",
-        bg: "fcf1ed",
+        bg: "fcf1ed"
     },
     {
         id: 3,
@@ -19,5 +19,58 @@ export const sliderItems = [
         title: "Популярно",
         desc: "Не бойтесь экспериментировать! Получите скидку 30% на новые поступления.",
         bg: "fbf0f4"
+    }
+]
+
+export const categories = [
+    {
+        id: 1,
+        img: "https://s-kupe.by/cache/com_zoo/images/ALEX3042_9a3e05f9fab8cdb47cf89a02e733893f.jpg",
+        title: "Шкафы"
+    },
+    {
+        id: 2,
+        img: "https://basket-01.wb.ru/vol133/part13309/13309746/images/big/1.jpg",
+        title: "Стулья"
+    },
+    {
+        id: 3,
+        img: "https://mebelny-shchit.by/files/resized/1000x1000-35-obed-stol.jpg",
+        title: "Столы"
+    }
+]
+
+export const popularProducts = [
+    {
+        id: 1,
+        img: "https://lordmebel.com.ua/images/tovar/tovar380/tumba-dlja-obuvi-ijuks-44-XHWCU0nfghj.jpg"
+    },
+    {
+        id: 2,
+        img: "https://chester.by/img/works/puf-s-karetnoj-styazhkoj-2.jpg"
+    },
+    {
+        id: 3,
+        img: "https://www.telemagazin.by/wp-content/uploads/2020/10/polka-veshalka-telemagazin_03-600x800.jpg"
+    },
+    {
+        id: 4,
+        img: "https://mexo.by/assets/images/catalog/pufy/anatol/anatol.jpg"
+    },
+    {
+        id: 5,
+        img: "https://images.deal.by/288309545_stellazh-kub-2-fabrika.jpg"
+    },
+    {
+        id: 6,
+        img: "https://www.sokol-mebel.ru/global_images/goods/c4c/19c/c4c/bda/1144916.jpg"
+    },
+    {
+        id: 7,
+        img: "https://pinskdrev.by/web/catalogfiles/photogallery/Offer/7711/kanyon_stol.jpg"
+    },
+    {
+        id: 8,
+        img: "https://static.insales-cdn.com/images/products/1/7774/362233438/dms_pn008_600_8685_interior2_101217.jpg"
     }
 ]
